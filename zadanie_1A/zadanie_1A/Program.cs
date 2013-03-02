d1483a1b6089d4db47c9e8773f5c4370f884a3a7
Tomek Janiszewski <janiszt@gmail.com>
2013-03-02 17:04:16 +0100

    Add program skeleton

--- zadanie_1A/zadanie_1A/Program.cs
+++ zadanie_1A/zadanie_1A/Program.cs
@@ -6,6 +6,56 @@ using System.Threading.Tasks;
 
 namespace zadanie_1A
 {
+
+    public enum CookType
+    {
+        Desserts,
+        Soups,
+        MainCourses
+    }
+
+    class Peoples
+    {
+        private static Random random = new Random();
+        private const int MinPreparationTime = 1;
+        private const int MaxPreparationTime = 500;
+
+        private static int prepareTime
+        {
+            get 
+            { 
+                return random.Next(MinPreparationTime, MaxPreparationTime); 
+            }
+        }
+
+        public static void Cook(CookType type)
+        {
+
+        }
+
+        public static void Miner()
+        {
+
+        }
+
+        public static void Child()
+        {
+
+        }
+
+        public static void Woman()
+        {
+
+        }
+
+
+    }
+
+    class Line
+    {
+
+    }
+
     class Program
     {
         static void Main(string[] args)

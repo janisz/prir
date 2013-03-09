f3f1c5abdfb20991c7a7132a21b8c13718eaf5ce
Tomek Janiszewski <janiszt@gmail.com>
2013-03-09 17:21:52 +0100

    Put classes into separated files

---
+++ zadanie_1A/zadanie_1A/Counter.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace zadanie_1A
+{
+    class Counter
+    {
+        private const int counterSize = 4;
+
+        private int womenCount = 0;
+
+        public Semaphore Women = new Semaphore(1, 1);
+
+        public Semaphore DessertsFull = new Semaphore(0, counterSize);
+        public Semaphore SoupsFull = new Semaphore(0, counterSize);
+        public Semaphore MainCoursesFull = new Semaphore(0, counterSize);
+
+        public Semaphore DessertsEmpty = new Semaphore(counterSize, counterSize);
+        public Semaphore SoupsEmpty = new Semaphore(counterSize, counterSize);
+        public Semaphore MainCoursesEmpty = new Semaphore(counterSize, counterSize);
+
+        public int getWomenCount()
+        {
+            Women.WaitOne();
+            int ret = womenCount;
+            Women.Release();
+            return ret;
+        }
+
+        public int incWomenCount()
+        {
+            Women.WaitOne();
+            int ret = ++womenCount;
+            Women.Release();
+            return ret;
+        }
+
+        public void decWomenCount()
+        {
+            Women.WaitOne();
+            womenCount--;
+            Women.Release();
+        }
+
+    }
+}

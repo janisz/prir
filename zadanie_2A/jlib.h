ab6f65c4d4bf3d6887f1157a12b8718d3521a0a0
Tomek Janiszewski <janiszt@gmail.com>
2013-04-06 17:58:45 +0200

    Add constant definitions

--- zadanie_2A/jlib.h
+++ zadanie_2A/jlib.h
@@ -1,1 +1,12 @@
+#include <stdlib.h>
 
+#define KING 0
+//Time in miliseconds
+#define WAITER_INTERVAL 1000
+#define MAX_TALKING_TIME 500
+#define MIN_TALKING_TIME 100
+#define TALKING_TIME ((rand() % (MAX_TALKING_TIME - MIN_TALKING_TIME)) + MIN_TALKING_TIME)
+
+
+#define TRUE (!FALSE)
+#define FALSE (0)

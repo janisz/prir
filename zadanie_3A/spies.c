267c2b92f5b06ba5e0c718c7e16a32541250341f
Tomek Janiszewski <janiszt@gmail.com>
2013-05-04 19:33:43 +0200

    Fix typo

--- zadanie_3A/spies.c
+++ zadanie_3A/spies.c
@@ -12,41 +12,41 @@ int main( int argc, char *argv[] )
 		exit(EXIT_FAILURE);
 	}
 		
-	int vertexs_count;
+	int vertices_count;
 	
 	FILE *f = fopen(argv[1], "r");
 	char line[LINE_SIZE];
 	fgets(line, LINE_SIZE, f);
-	sscanf(line, "%d", &vertexs_count); 
-	int edges_count = vertexs_count -1;
+	sscanf(line, "%d", &vertices_count); 
+	int edges_count = vertices_count -1;
 	
-	printf("%d %d\n", vertexs_count, edges_count);
+	printf("%d %d\n", vertices_count, edges_count);
 	
-	int index[vertexs_count];
+	int index[vertices_count];
 	int edges[edges_count];
-	int matrix_graph[vertexs_count * vertexs_count];
-	for (int i=0;i<vertexs_count*vertexs_count;i++)
+	int matrix_graph[vertices_count * vertices_count];
+	for (int i=0;i<vertices_count*vertices_count;i++)
 		matrix_graph[i] = 0;
 	for (int i=0; i<edges_count;i++) {
 		int from, to;
 		fgets(line, LINE_SIZE, f);
 		sscanf(line, "%d %d", &from, &to);
-		matrix_graph[to*vertexs_count+from] = 1;
+		matrix_graph[to*vertices_count+from] = 1;
 	}
 	
-	for (int i=0; i<vertexs_count;i++) {
-		for (int j=0; j<vertexs_count;j++) {
-			matrix_graph[i*vertexs_count+j] = matrix_graph[i*vertexs_count+j] != 1 ? 0 : 1;
-			printf("%d ", matrix_graph[i*vertexs_count+j]);
+	for (int i=0; i<vertices_count;i++) {
+		for (int j=0; j<vertices_count;j++) {
+			matrix_graph[i*vertices_count+j] = matrix_graph[i*vertices_count+j] != 1 ? 0 : 1;
+			printf("%d ", matrix_graph[i*vertices_count+j]);
 		}
 		printf("\n");
 	}
 	
 	int edge_index = 0;
-	for (int i=0; i<vertexs_count;i++) {
+	for (int i=0; i<vertices_count;i++) {
 		int neighbours = 0;
-		for (int j=0; j<vertexs_count;j++) {			
-			if (matrix_graph[j*vertexs_count+i] == 1) {
+		for (int j=0; j<vertices_count;j++) {			
+			if (matrix_graph[j*vertices_count+i] == 1) {
 				neighbours++;
 				edges[edge_index] = j;
 				edge_index++;				
@@ -57,7 +57,7 @@ int main( int argc, char *argv[] )
 	
 	printf("Node \t index \t edges\n");
 	edge_index = 0;
-	for (int i=0; i<vertexs_count;i++) {
+	for (int i=0; i<vertices_count;i++) {
 		printf("%d \t %d \t", i, index[i]);
 		int current_index = index[i];
 		for (int i = edge_index; i < current_index; i++)

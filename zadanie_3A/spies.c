d70acb03daf8af52d3d88262ada7389b319dbc80
Tomek Janiszewski <janiszt@gmail.com>
2013-05-05 11:29:08 +0200

    Early alpha version

--- zadanie_3A/spies.c
+++ zadanie_3A/spies.c
@@ -1,83 +1,183 @@
 #include <mpi.h>
+#include <time.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include <unistd.h>
+#include <sys/stat.h>
+#include <sys/types.h>
 
 #define FILENAME_SIZE 20
 #define LINE_SIZE 80
 
+#define REQUEST 0
+#define REPORT 1
+
+typedef struct {
+    int time;
+    int count;
+} Data;
+
+int rank, size;
+MPI_Comm comm;
+
 int main( int argc, char *argv[] )
 {
-    int rank, size;
-
     MPI_Init( &argc, &argv );
     MPI_Comm_rank( MPI_COMM_WORLD, &rank );
     MPI_Comm_size( MPI_COMM_WORLD, &size );
     printf( "Hello world from process %d of %d\n", rank, size );
-    if (rank == 0) {
 
-        if (argc != 2) {
-            printf("Specify graph file");
-            exit(EXIT_FAILURE);
-        }
+	srand(time(0) * rank);    
 
-        int vertices_count;
 
-        FILE *f = fopen(argv[1], "r");
-        char line[LINE_SIZE];
+    if (argc != 2) {
+        printf("Specify graph file");
+        exit(EXIT_FAILURE);
+    }
+
+    int vertices_count;
+
+    FILE *f = fopen(argv[1], "r");
+    if (!f) {
+		MPI_Abort(MPI_COMM_WORLD, 10);
+    }
+    char line[LINE_SIZE];
+    fgets(line, LINE_SIZE, f);
+    sscanf(line, "%d", &vertices_count);
+    int edges_count = vertices_count -1;
+
+    printf("%d %d\n", vertices_count, edges_count);
+
+    int index[vertices_count];
+    int edges[edges_count];
+    int matrix_graph[vertices_count * vertices_count];
+    for (int i=0; i<vertices_count*vertices_count; i++)
+        matrix_graph[i] = 0;
+    for (int i=0; i<edges_count; i++) {
+        int from, to;
         fgets(line, LINE_SIZE, f);
-        sscanf(line, "%d", &vertices_count);
-        int edges_count = vertices_count -1;
+        sscanf(line, "%d %d", &from, &to);
+        matrix_graph[to*vertices_count+from] = 1;
+    }
+	
+	fclose(f);
 
-        printf("%d %d\n", vertices_count, edges_count);
-
-        int index[vertices_count];
-        int edges[edges_count];
-        int matrix_graph[vertices_count * vertices_count];
-        for (int i=0; i<vertices_count*vertices_count; i++)
-            matrix_graph[i] = 0;
-        for (int i=0; i<edges_count; i++) {
-            int from, to;
-            fgets(line, LINE_SIZE, f);
-            sscanf(line, "%d %d", &from, &to);
-            matrix_graph[to*vertices_count+from] = 1;
+    for (int i=0; i<vertices_count; i++) {
+        for (int j=0; j<vertices_count; j++) {
+            matrix_graph[i*vertices_count+j] = matrix_graph[i*vertices_count+j] != 1 ? 0 : 1;
+            printf("%d ", matrix_graph[i*vertices_count+j]);
         }
+        printf("\n");
+    }
 
-        for (int i=0; i<vertices_count; i++) {
-            for (int j=0; j<vertices_count; j++) {
-                matrix_graph[i*vertices_count+j] = matrix_graph[i*vertices_count+j] != 1 ? 0 : 1;
-                printf("%d ", matrix_graph[i*vertices_count+j]);
+    int edge_index = 0;
+    for (int i=0; i<vertices_count; i++) {
+        int neighbours = 0;
+        for (int j=0; j<vertices_count; j++) {
+            if (matrix_graph[j*vertices_count+i] == 1) {
+                neighbours++;
+                edges[edge_index] = j;
+                edge_index++;
             }
-            printf("\n");
         }
+        index[i] = edge_index;
+    }
 
-        int edge_index = 0;
-        for (int i=0; i<vertices_count; i++) {
-            int neighbours = 0;
-            for (int j=0; j<vertices_count; j++) {
-                if (matrix_graph[j*vertices_count+i] == 1) {
-                    neighbours++;
-                    edges[edge_index] = j;
-                    edge_index++;
-                }
-            }
-            index[i] = edge_index;
-        }
-
-        printf("Node \t index \t edges\n");
-        edge_index = 0;
-        for (int i=0; i<vertices_count; i++) {
-            printf("%d \t %d \t", i, index[i]);
-            int current_index = index[i];
-            for (int i = edge_index; i < current_index; i++)
-                printf("%d ", edges[i]);
-            printf("\n");
-            edge_index = index[i];
-        }
+    printf("Node \t index \t edges\n");
+    edge_index = 0;
+    for (int i=0; i<vertices_count; i++) {
+        printf("%d \t %d \t", i, index[i]);
+        int current_index = index[i];
+        for (int i = edge_index; i < current_index; i++)
+            printf("%d ", edges[i]);
+        printf("\n");
+        edge_index = index[i];
 
     }
 
+    MPI_Graph_create( MPI_COMM_WORLD, vertices_count, index, edges, 0, &comm );
+
+
     MPI_Barrier(MPI_COMM_WORLD);
 
+	int neighbors_count = 0;
+	MPI_Graph_neighbors_count(comm, rank, &neighbors_count);
+	printf("#%d has %d neighbors\n", rank, neighbors_count); 
+ 
+	 
+	if (neighbors_count == 0) {
+	
+		Data raport;
+		MPI_Status status;
+		MPI_Request request;
+				
+		MPI_Irecv(&raport, 1, MPI_2INT, MPI_ANY_SOURCE, REQUEST, comm, &request);
+		int sleep_time = rand()%5+3;
+		printf("#%d spying for %d s\n", rank, sleep_time);
+		sleep(sleep_time);
+		
+		raport.time = time(0);
+		raport.count = rand()%100;
+		
+		MPI_Wait(&request, &status);
+		MPI_Isend(&raport, 1, MPI_2INT, status.MPI_SOURCE, REPORT, comm, &request);
+		printf("#%d sent raport to #%d:  %d ; %d\n", rank, status.MPI_SOURCE, raport.time, raport.count);
+		MPI_Wait(&request, &status);
+		printf("#%d finished his job", rank);		
+	}
+	else {
+		Data raport;
+		Data reports[neighbors_count];
+		int head;
+		int neighbors[neighbors_count];
+		MPI_Status statuses[neighbors_count];
+		MPI_Request requests[neighbors_count];					
+		
+		MPI_Request request;
+		MPI_Status status;
+		
+		MPI_Graph_neighbors(comm, rank, neighbors_count, neighbors);
+		
+		if (rank != 0) {
+			MPI_Irecv(&head, 1, MPI_INT, MPI_ANY_SOURCE, REQUEST, comm, &request);
+			MPI_Wait(&request, &status);
+		}
+		
+		for (int i = 0; i < neighbors_count; i++) {
+			MPI_Isend(&rank, 1, MPI_INT, neighbors[i], REQUEST, comm, &requests[i]);
+			printf("#%d sent raport request to #%d", rank, neighbors[i]);
+		}
+		
+		sleep(rand()%6+2);
+		
+		printf("#%d start reports gathering\n", rank);
+		
+		for (int i = 0; i < neighbors_count; i++) {
+			MPI_Irecv(&reports[i], 1, MPI_2INT, neighbors[i], REPORT, comm, &requests[i]);
+		}
+		
+		MPI_Waitall(neighbors_count, requests, statuses);
+		raport.time = -1;
+		raport.count = -1;
+		
+		for (int i = 0; i < neighbors_count; i++) {
+			if (raport.time < reports[i].time || 
+			(raport.time == reports[i].time && raport.count < reports[i].count)) {
+				raport.time = reports[i].time;
+				raport.count = reports[i].count;
+			}
+		}
+		
+		if (rank) {
+			printf("#%d sent raport to #%d:  %d ; %d\n", rank, status.MPI_SOURCE, raport.time, raport.count);
+			MPI_Isend(&raport, 1, MPI_2INT, status.MPI_SOURCE, REPORT, comm, &request);
+			MPI_Wait(&request, &status);
+		}
+		else {
+			printf("Final raport:  %d ; %d\n", raport.time, raport.count);			
+		}
+	}
+ 
     MPI_Finalize();
 
     return 0;

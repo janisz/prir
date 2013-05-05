fc685ecbaa397df0c5ec2cf1de00999f8d2e3f09
Tomek Janiszewski <janiszt@gmail.com>
2013-05-05 17:25:58 +0200

    Only one process parse file

--- zadanie_3A/spies.c
+++ zadanie_3A/spies.c
@@ -27,79 +27,95 @@ int main( int argc, char *argv[] )
     MPI_Comm_size( MPI_COMM_WORLD, &size );
     printf( "Hello world from process %d of %d\n", rank, size );
 
-	srand(time(0)*rank);    
-
-
-    if (argc != 2) {
-        printf("Specify graph file");
-        exit(EXIT_FAILURE);
-    }
-
+	srand(time(0)*rank);   
+	
+	int *index;
+    int *edges;
+    int edges_count;
     int vertices_count;
 
-    FILE *f = fopen(argv[1], "r");
-    if (!f) {
-		MPI_Abort(MPI_COMM_WORLD, 10);
-    }
-    char line[LINE_SIZE];
-    fgets(line, LINE_SIZE, f);
-    sscanf(line, "%d", &vertices_count);
-    int edges_count = vertices_count -1;
+	if (!rank) {
+		FILE *f;
+		if (argc != 2) {
+			f = fopen("./siatka", "r");
+		} else {
+			f = fopen(argv[1], "r");
+		}
 
-    printf("%d %d\n", vertices_count, edges_count);
+		
 
-    int index[vertices_count];
-    int edges[edges_count];
-    int matrix_graph[vertices_count * vertices_count];
-    for (int i=0; i<vertices_count*vertices_count; i++)
-        matrix_graph[i] = 0;
-    for (int i=0; i<edges_count; i++) {
-        int from, to;
-        fgets(line, LINE_SIZE, f);
-        sscanf(line, "%d %d", &from, &to);
-        matrix_graph[to*vertices_count+from] = 1;
-    }
+		
+		if (!f) {
+			MPI_Abort(MPI_COMM_WORLD, 10);
+		}
+		char line[LINE_SIZE];
+		fgets(line, LINE_SIZE, f);
+		sscanf(line, "%d", &vertices_count);
+		edges_count = vertices_count -1;
+
+		printf("%d %d\n", vertices_count, edges_count);
+
+		index = (int*)malloc(sizeof(int) * vertices_count);
+		edges = (int*)malloc(sizeof(int) * edges_count);
+
+		int matrix_graph[vertices_count * vertices_count];
+		for (int i=0; i<vertices_count*vertices_count; i++)
+			matrix_graph[i] = 0;
+		for (int i=0; i<edges_count; i++) {
+			int from, to;
+			fgets(line, LINE_SIZE, f);
+			sscanf(line, "%d %d", &from, &to);
+			matrix_graph[to*vertices_count+from] = 1;
+		}
+		
+		fclose(f);
+
+		for (int i=0; i<vertices_count; i++) {
+			for (int j=0; j<vertices_count; j++) {
+				matrix_graph[i*vertices_count+j] = matrix_graph[i*vertices_count+j] != 1 ? 0 : 1;
+				printf("%d ", matrix_graph[i*vertices_count+j]);
+			}
+			printf("\n");
+		}
+
+		int edge_index = 0;
+		for (int i=0; i<vertices_count; i++) {
+			int neighbours = 0;
+			for (int j=0; j<vertices_count; j++) {
+				if (matrix_graph[j*vertices_count+i] == 1) {
+					neighbours++;
+					edges[edge_index] = j;
+					edge_index++;
+				}
+			}
+			index[i] = edge_index;
+		}
+
+		printf("Node \t index \t edges\n");
+		edge_index = 0;
+		for (int i=0; i<vertices_count; i++) {
+			printf("%d \t %d \t", i, index[i]);
+			int current_index = index[i];
+			for (int i = edge_index; i < current_index; i++)
+				printf("%d ", edges[i]);
+			printf("\n");
+			edge_index = index[i];
+
+		}
+		
+	}
 	
-	fclose(f);
+	MPI_Bcast(&vertices_count, 1, MPI_INT, 0, MPI_COMM_WORLD);
+	edges_count = vertices_count - 1;
+	if (rank) {
+		index = (int*)malloc(sizeof(int) * vertices_count);
+		edges = (int*)malloc(sizeof(int) * edges_count);
+	}
 
-    for (int i=0; i<vertices_count; i++) {
-        for (int j=0; j<vertices_count; j++) {
-            matrix_graph[i*vertices_count+j] = matrix_graph[i*vertices_count+j] != 1 ? 0 : 1;
-            printf("%d ", matrix_graph[i*vertices_count+j]);
-        }
-        printf("\n");
-    }
-
-    int edge_index = 0;
-    for (int i=0; i<vertices_count; i++) {
-        int neighbours = 0;
-        for (int j=0; j<vertices_count; j++) {
-            if (matrix_graph[j*vertices_count+i] == 1) {
-                neighbours++;
-                edges[edge_index] = j;
-                edge_index++;
-            }
-        }
-        index[i] = edge_index;
-    }
-
-    printf("Node \t index \t edges\n");
-    edge_index = 0;
-    for (int i=0; i<vertices_count; i++) {
-        printf("%d \t %d \t", i, index[i]);
-        int current_index = index[i];
-        for (int i = edge_index; i < current_index; i++)
-            printf("%d ", edges[i]);
-        printf("\n");
-        edge_index = index[i];
-
-    }
-
-    MPI_Graph_create( MPI_COMM_WORLD, vertices_count, index, edges, 0, &comm );
-
-
-    MPI_Barrier(MPI_COMM_WORLD);
-
+	MPI_Bcast(index, vertices_count, MPI_INT, 0, MPI_COMM_WORLD);
+	MPI_Bcast(edges, edges_count, MPI_INT, 0, MPI_COMM_WORLD);    
+	MPI_Graph_create( MPI_COMM_WORLD, vertices_count, index, edges, 0, &comm );
+	
 	int neighbors_count = 0;
 	MPI_Graph_neighbors_count(comm, rank, &neighbors_count);
 	printf("#%d has %d neighbors\n", rank, neighbors_count); 
